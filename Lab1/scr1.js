2862ec6836e2eb10002a900cbd1dbcee66581d21
Matei Tita <matei_dimitrie.tita@stud.acs.upb.ro>
2020-03-19 19:16:16 +0000

    Deleted Lab1/.gitkeep, Lab1/Lab, Lab1/ex2.js, Lab1/ex3.js, Lab1/ex4+5.js, Lab1/scr1.js, Lab1/scr2.js, Lab1/scr3.js, Lab1/scr4.js, Lab1/scr5.js, Lab1/scr6.js files

--- Lab1/scr1.js
+++
@@ -1,8 +0,0 @@
-function myFunc(x) {console.log(x)};
-
-// este identic cu
-
-const myFuncArrow = (x) => console.log(x);
-
-myFunc(3); //3
-myFuncArrow(3); //3
--- Lab1/scr4.js
+++
@@ -1,4 +0,0 @@
-const obj = {a:1, b:2};
-
-console.log(obj.a); //1
-console.log(obj['b']); //2
